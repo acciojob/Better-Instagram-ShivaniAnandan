dc8382d323a6f2a36549a1a3dae16243b297ff58
Shivani Anandan <124684724+ShivaniAnandan@users.noreply.github.com>
2023-10-01 20:12:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+const divs = document.querySelectorAll('.image');
+
+for (const div of divs) {
+  div.addEventListener('dragstart', (e) => {
+    e.dataTransfer.setData('image', div.id);
+  });
+
+  div.addEventListener('dragover', (e) => {
+    e.preventDefault();
+  });
+
+  div.addEventListener('drop', (e) => {
+    const imageId = e.dataTransfer.getData('image');
+    const div1;
+  });
+}
